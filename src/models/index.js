b9bd1e9f458651b2dcbf2f47d3386e074e385032
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2025-01-05 11:28:23 +0700

    refactor: improve migration scripts, database configuration, and model integration
    - Adjusted migration and seed scripts for better data handling.
    - Updated database configuration for improved connection management.
    - Modified models/index.js to enhance integration of Sequelize models.

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,16 @@
 import fs from "fs";
-import process from "process";
 import path from "path";
+import { fileURLToPath } from "url";
+import process from "process";
 import { Sequelize, DataTypes } from "sequelize";
 import databaseConfig from "../config/database.config.js";
+import { NODE_ENV } from "../utils/env.util.js";
 
-const basename = path.basename(import.meta.url);
-const env = process.env.NODE_ENV || "development";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const basename = path.basename(__filename);
+const env = NODE_ENV || "development";
 const dbConfig = databaseConfig[env];
 const db = {};
 
@@ -21,24 +26,31 @@ if (dbConfig.use_env_variable) {
   );
 }
 
-const modelFiles = fs.readdirSync(path.resolve("src/models")).filter((file) => {
+const modelFiles = fs.readdirSync(__dirname).filter((file) => {
   return (
     file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
   );
 });
 
-for (const file of modelFiles) {
-  const model = (
-    await import(path.join(path.dirname(import.meta.url), file))
-  ).default(sequelize, DataTypes);
-  db[model.name] = model;
+async function loadModels() {
+  for (const file of modelFiles) {
+    const modelPath = path.join(__dirname, file);
+    const model = (await import(`file://${modelPath}`)).default(
+      sequelize,
+      DataTypes
+    );
+    db[model.name] = model;
+  }
+
+  Object.keys(db).forEach((modelName) => {
+    if (db[modelName].associate) {
+      db[modelName].associate(db);
+    }
+  });
 }
 
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+await loadModels();
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
